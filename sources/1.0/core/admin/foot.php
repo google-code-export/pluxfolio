d3fe3927a5a62bfa6a3a3731a753fb2a2e5b49b7
bender.rodriges <bender.rodriges@548d73a2-1a84-e572-0757-7695e96365bb>
2011-03-28 14:35:45 +0000

    RC1

---
+++ sources/1.0/core/admin/foot.php
@@ -0,0 +1,5 @@
+</div><div id="navigation2">
+		<p id="footer" style="clear: both;"><?php echo $ADM_poweredby_title;?> <a href="http://pluxfolio.ru/">Pluxfolio</a> <?php echo $plxAdmin->version; ?></p>
+	</div>
+</body>
+</html>
