0c70879fff09872d4db1732bae16d5096899a14e
bender.rodriges <bender.rodriges@548d73a2-1a84-e572-0757-7695e96365bb>
2011-03-28 14:47:40 +0000

    RC2

---
+++ sources/1.0/core/lib/class.plx.encrypt.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Classe plxEncrypt responsable du cryptage et décryptage de données
+ *
+ * @package PLX
+ * @author	Stephane F
+ **/
+ 
+define('ENCRYPTION_KEY', 'ab12cd34#$');
+
+class plxEncrypt {
+
+	function base64url_encode($plainText) {
+
+		$base64 = base64_encode($plainText);
+		$base64url = strtr($base64, '+/=', '-_,');
+		return $base64url;  
+	}
+
+	function base64url_decode($plainText) {
+
+		$base64url = strtr($plainText, '-_,', '+/=');
+		$base64 = base64_decode($base64url);
+		return $base64;  
+	}
+
+	function encryptId($int, $class='') {
+
+		return plxEncrypt::base64url_encode($int.'*'.substr(sha1($class.$int.ENCRYPTION_KEY), 0, 6));
+	}
+
+	function decryptId($int, $class='') {
+
+		$parts = explode('*', plxEncrypt::base64url_decode($int));
+		if(count($parts) != 2) 
+			return 0;
+		return substr(sha1($class.$parts[0].ENCRYPTION_KEY), 0, 6) === $parts[1] ? $parts[0] : 0;
+	}
+
+}
+?>
